e011dbbbb0215cc1252ce413b7c89923d1797263
Ezequiel Murillo <ezequiel.murillo@wizeline.com>
2021-11-14 20:55:07 -0600

    header logo set to css media query

--- src/Header.js
+++ src/Header.js
@@ -4,31 +4,35 @@ import {BsFillCartFill} from 'react-icons/bs'
 const headerStyle = {
   paddingTop: "10px"
 }
-const logoStyle = {
-  height: "40px",
-}
+
 const textInputStyle = {
   border: "1px solid #ccc",
   borderRadius: "4px",
-  paddingLeft: "10px",
   height: "35px",
-  position: "absolute",
-  right: "150px",
+  paddingLeft: "10px",
+  float: "left"
 }
 const cartStyle = {
+  fontSize: "27px",
+  float: "right",
+  margin: "3px 0 0 10px"
+}
+const headerRight = {
+  float: "right",
   position: "absolute",
-  top: "15px",
-  right: "100px",
-  fontSize: "25px",
+  right: "8%",
+  top: "10px"
 }
 
 function Header () {
     return (
       <div className="Header" style={headerStyle}>
 
-        <MoblyLogo style={logoStyle}/>
-        <input type="text" placeholder="Seach..." style={textInputStyle}/>
-        <BsFillCartFill style={cartStyle}/>
+        <MoblyLogo className="header-logo" />
+        <div style={headerRight}>
+          <input type="text" placeholder="Seach..." style={textInputStyle}/>
+          <BsFillCartFill style={cartStyle}/>
+        </div>
  
       </div>
     );
