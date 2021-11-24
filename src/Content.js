8a8210e12799072fe68bbc0e26052206343570b3
Ezequiel Murillo <ezequiel.murillo@wizeline.com>
2021-11-24 01:54:36 -0600

    Cobverted view products to component

--- src/Content.js
+++ src/Content.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 
 import banners from './data/featured-banners.json'
 import categories from './data/product-categories.json'
@@ -9,22 +8,13 @@ import ImageSlider from './components/ImageSlider'
 import ImageGrid from './components/ImageGrid'
 import Products from './components/Products'
 import ProductList from './components/ProductList'
+import ViewProductsButton from './components/ViewProductsButton'
 
-const ShowButton = styled.span`
-  position: absolute;
-  background-color: #d8ab196c;
-  padding: 10px;
-  cursor: pointer;
-  z-index: 2;
-  &:hover {
-    background-color: #9e801eb7;
-  }
-`
 
 function Home({setShowProducts}) {
   return (
     <React.Fragment>
-      <ShowButton onClick={setShowProducts}>View all products</ShowButton>
+      <ViewProductsButton/>
       <ImageSlider banners={banners.results} />
       <ImageGrid categories={categories.results} />
       <Products products={products.results} />
